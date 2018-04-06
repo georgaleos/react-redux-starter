e1ca141574b6c307ebc6114e820477d6eb5a5350
Georgios Eleftheriadis <georgaleos232@gmail.com>
2018-04-07 01:48:57 +0300

    * Added a Header component to use for routing * Created a UsersPage component * Created styles.css

---
+++ src/components/user/UsersPage.js
@@ -0,0 +1,13 @@
+import React, {PropTypes} from 'react';
+
+class UsersPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Users</h1>
+            </div>
+        );
+    }
+}
+
+export default UsersPage;

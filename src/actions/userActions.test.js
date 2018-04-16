4936065390a9001f49e5e079df43a45a2af627f6
Georgios Eleftheriadis <georgaleos232@gmail.com>
2018-04-16 11:33:02 +0300

    * Created thunk test when loading users

--- src/actions/userActions.test.js
+++ src/actions/userActions.test.js
@@ -2,6 +2,10 @@ import expect from 'expect';
 import * as userActions from './userActions';
 import * as types from './actionTypes';
 
+import thunk from 'redux-thunk';
+import nock from 'nock';
+import configureMockStore from 'redux-mock-store';
+
 // Test a sync action
 describe('User Actions', () => {
     describe('createUserSuccess', () => {
@@ -20,4 +24,34 @@ describe('User Actions', () => {
             expect(action).toEqual(expectedAction);
         });
     });
+});
+
+
+const middleware = [thunk];
+const mockStore = configureMockStore(middleware);
+
+describe('Async Actions', () => {
+   afterEach(()=> {
+       nock.cleanAll();
+   });
+
+   it('should create BEGIN_AJAX_CALL and LOAD_USERS_SUCCESS when loading users', (done) => {
+       // Here's an example call to nock.
+       // nock('http://example.com/')
+       //   .get('/users')
+       //   .reply(200, { body: { users: [{ id: 'george-ele', firstName: 'George', lastName: 'Ele', managerId: ''}]}});
+
+       const expectedActions = [
+           {type: types.BEGIN_AJAX_CALL},
+           {type: types.LOAD_USERS_SUCCESS, body: {users: [{ id: 'george-ele', firstName: 'George', lastName: 'Ele', managerId: ''}]}}
+       ];
+
+       const store = mockStore({users: []}, expectedActions);
+       store.dispatch(userActions.loadUsers()).then(() => {
+           const actions = store.getActions();
+           expect(actions[0].type).toEqual(types.BEGIN_AJAX_CALL);
+           expect(actions[1].type).toEqual(types.LOAD_USERS_SUCCESS);
+           done();
+       });
+   });
 });
